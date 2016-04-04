dab32d1b9e0718d3079f4edc6054604bee264c54
toxa81 <toxa81@gmail.com>
2016-04-04 16:28:43 +0200

    try blocks

--- test_io/main.cpp
+++ test_io/main.cpp
@@ -3,6 +3,42 @@
 #include "runtime.h"
 #include "communicator.h"
 
+void write_in_blocks()
+{
+    std::stringstream s;
+    s << "test." << mpi_comm_world().rank() << ".bin";
+
+    std::vector<double> v(1 << 20, 0);
+
+    int N = 500;
+    
+    runtime::Timer t("fwrite");
+    for (int i = 0; i < mpi_comm_world().size(); i++)
+    {
+        if (mpi_comm_world().rank() == i)
+        {
+            
+            for (int j = 0; j < N; j++)
+            {
+                runtime::Timer t1("fwrite.chunk");
+                FILE* fout = fopen(s.str().c_str(), "a");
+                fwrite(&v[0], sizeof(double), v.size(), fout);
+                fclose(fout);
+                double tval = t1.stop();
+                printf("%i %f\n", j, v.size() * sizeof(double) / double(1 << 30) / tval);
+            }
+        }
+        mpi_comm_world().barrier();
+    }
+    double tval = t.stop();
+    
+    if (mpi_comm_world().rank() == 0)
+    {
+        printf("sequential io speed: %f (Gb/sec.)\n", mpi_comm_world().size() * N * v.size() * sizeof(double) / double(1 << 30) / tval);
+    }
+
+}
+
 void write_sequential()
 {
     std::stringstream s;
@@ -25,7 +61,7 @@ void write_sequential()
     
     if (mpi_comm_world().rank() == 0)
     {
-        printf("sequential io speed: %f (Gb/sec.)i\n", mpi_comm_world().size() * v.size() * sizeof(double) / double(1 << 30) / tval);
+        printf("sequential io speed: %f (Gb/sec.)\n", mpi_comm_world().size() * v.size() * sizeof(double) / double(1 << 30) / tval);
     }
 }
 
@@ -33,7 +69,9 @@ int main(int argn, char** argv)
 {
     Communicator::initialize();
 
-    write_sequential();
+    write_in_blocks();
+
+    //write_sequential();
 
     //std::stringstream s;
     //s << "test." << mpi_comm_world().rank() << ".bin";
