1caa9fe2043462c44875e48501e047959174621a
toxa81 <toxa81@gmail.com>
2020-03-19 11:14:09 +0100

    test of libm

---
+++ test_libm/test_libm.f90
@@ -0,0 +1,45 @@
+program test_libm
+implicit none
+complex(8), allocatable :: a(:)
+integer n
+real(8), external :: omp_get_wtime
+real(8) :: t
+complex(8) :: z
+integer j
+!
+n = 10000000
+allocate(a(n))
+!
+t = -omp_get_wtime()
+do j = 1, 10
+  call run_test(a, n, z)
+enddo
+t = t + omp_get_wtime()
+!
+write(*,*)'time=',t,' sec.'
+write(*,*)'z=',z
+
+deallocate(a)
+
+end program
+
+subroutine run_test(a, n, z)
+implicit none
+integer, intent(in) :: n
+complex(8), intent(inout) :: a(n)
+complex(8), intent(out) :: z
+integer :: i
+real(8) d,p
+
+z = 0.d0
+d = 1.d0 / n
+do i = 1, n
+  p = d * i * 2 * 3.141592d0
+  a(i) = exp(cmplx(0.d0, p))
+enddo
+do i = 1, n
+  z = z + a(i)
+enddo
+
+
+end subroutine
